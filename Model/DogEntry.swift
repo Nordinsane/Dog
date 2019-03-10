9e0869d49cb89d033cc182f229299eb0fdad3cfd
Nordinsane <nordinsane@gmail.com>
2019-03-10 14:56:10 +0100

    Dog

---
+++ Model/DogEntry.swift
@@ -0,0 +1,21 @@
+//
+//  DogEntry.swift
+//  Dog
+//
+//  Created by Kim Nordin on 2019-03-10.
+//  Copyright © 2019 kim. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DogEntry {
+    
+    var name: String
+    var image: UIImage
+    
+    init(name: String, image: UIImage) {
+        self.name = name
+        self.image = image
+   }
+}
